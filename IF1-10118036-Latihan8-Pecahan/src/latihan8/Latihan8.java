cca39451e5148ddbba5d2e27eeb8660cf6bb780d
Adrian C. Senna <55816324+Raykitsuu@users.noreply.github.com>
2019-09-26 19:26:52 +0700

    Add files via upload

---
+++ IF1-10118036-Latihan8-Pecahan/src/latihan8/Latihan8.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan8;
+
+/**
+ *
+ * @author
+ * NAMA			: Adrian C. Senna
+ * KELAS		: IF-1
+ * NIM			: 10118036
+ * Deskripsi Program	: Program ini berisi program untuk memperlihatkan variable pecahan.
+ */
+public class Latihan8 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		float f;
+		double d;
+		f = 123456789.123456789F;
+		d = 1_234_567_890.123456789;
+		System.out.println("Float : " + f);	
+		System.out.println("Double : " + d);
+	}
+	
+}
